8ac31fb28305e9a46c4c0f7a14e4f06923b4fb9d
alevscyg <110639375+alevscyg@users.noreply.github.com>
2024-02-08 06:38:02 +0500

    Update userUK.dto.ts

--- userUK.dto.ts
+++ userUK.dto.ts
@@ -1,4 +1,4 @@
-//import { IsNumber, IsString, Length } from "class-validator";
+//import { IsString, Length } from "class-validator";
 export class UKDto{
     //@IsString({message: 'Must be a string'})
     readonly Строка_1:string;
