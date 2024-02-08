10a1559098f7e0ecffb5dc0d236e935a758f5eb6
alevscyg <110639375+alevscyg@users.noreply.github.com>
2024-02-08 06:36:01 +0500

    Update userRUS.dto.ts

--- userRUS.dto.ts
+++ userRUS.dto.ts
@@ -1,6 +1,6 @@
 //import { IsNumber, IsString, Length } from "class-validator";
 export class RUSDto{
-    //@IsString({message: 'Must be a string'})
+    //@IsString({message: 'Должно быть строкой'})
     readonly Строка_1:string;
     //@Length(6,6,{message:'Некорретный почтовый индекс'})
     //@IsNumber({},{message:'Должно быть числом'})
